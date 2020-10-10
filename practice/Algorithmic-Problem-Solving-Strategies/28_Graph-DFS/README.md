7d8a1b6c97d7258e1769bb17e321c1ad83b84d6c
User <user@Users-MacBook-Pro.local>
2020-10-10 20:51:20 +0900

    DFS

--- practice/Algorithmic-Problem-Solving-Strategies/28_Graph-DFS/README.md
+++ practice/Algorithmic-Problem-Solving-Strategies/28_Graph-DFS/README.md
@@ -47,3 +47,31 @@
 		- Whenever dfs(i) finishes, save the number of the current vertex.
 		- After calling dfsAll(), reverse the order of the saved numbers. It will be the topological-sorted result.
 	- It is expressed as a DAG. (no-cycle)
+	- [DICTIONARY](./DICTIONARY.cpp)
+- [Euler Circuit](https://jlmartin.ku.edu/~jlmartin/courses/math105-F11/Lectures/chapter5-part2.pdf)
+	- Use every edge of a graph `exactly once`.
+	- The source vertex is the same as the destination vertex.
+	- Each vertex in an euler circuit has even edges.
+	- If it has odd edges, the graph can not be an euler circuit.
+	- Time complexity = O(|V| * |E|)
+	
+	``` c++
+	vector<vector<int> > adj; // adjacency list
+
+	// if there is an undirected graph, return an euler circuit.
+	// get the euler circuit by reversing the returned.
+	void getEulerCircuit(int here, vector<int> &circuit) {
+		for (int there = 0; there < adj.size(); there++) {
+			while (adj[here][there] > 0) {
+				adj[here][there]--; // remove the passed edge.
+				adj[there][here]--;
+				getEulerCircuit(there, circuit);
+			}
+		}
+		circuit.push_back(here);
+	}
+	```
+
+	- If the graph has two more components(subsets), a component can be an euler circuit, but not the graph.
+	- _Euler Path_ starts and ends at different vertexes. Likewise, the path uses every edge of a graph exactly once.
+	- If you want to find out the euler path, add the edge from source to destination, and then call `getEulerCircuit()`. After removing the edge, you can get the euler path.
---
+++ practice/Algorithmic-Problem-Solving-Strategies/28_Graph-DFS/WORDCHAIN.cpp
@@ -0,0 +1,128 @@
+// https://algospot.com/judge/problem/read/WORDCHAIN
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define toIdx(x) x - 'a'
+#define ALPHABETS 26
+using namespace std;
+
+int n;
+// adj[i][j] = the number of edges from i to j.
+vector<vector<int> > adj;
+// graph[i][j] = the list of words starting from i and ending to j.
+vector<string> graph[ALPHABETS][ALPHABETS];
+// indegree[i] = the number of words starting from i.
+vector<int> indegree;
+// outdegree[i] = the number of words ends to j.
+vector<int> outdegree;
+
+
+void makeGraph(const vector<string> &words) {
+    // Initialize.
+    for (int i=0; i<ALPHABETS; i++)
+        for (int j=0; j<ALPHABETS; j++)
+            graph[i][j].clear();
+    
+    adj = vector<vector<int> > (ALPHABETS, vector<int> (ALPHABETS, 0));
+    indegree = outdegree = vector<int> (ALPHABETS, 0);
+    
+    // Make graph.
+    // Notice that we should add the edge, (b, a), to find the euler path beginning at a and finishing at b.
+    for (int i=0; i<words.size(); i++) {
+        int a = toIdx(words[i][0]);
+        int b = toIdx(words[i][words[i].size()-1]);
+        graph[a][b].push_back(words[i]);
+        adj[a][b]++;
+        outdegree[a]++;
+        indegree[b]++;
+    }
+}
+
+void getEulerCircuit(int here, vector<int> &circuit) {
+    for (int there=0; there<adj.size(); there++) {
+        while(adj[here][there] > 0) {
+            // This is a directed graph, so remove one direction.
+            adj[here][there]--;
+            getEulerCircuit(there, circuit);
+        }
+    }
+    circuit.push_back(here);
+}
+
+vector<int> getEulerPathOrCircuit() {
+    vector<int> circuit;
+    
+    // In case of existing the start,
+    for (int i=0; i<ALPHABETS; i++) {
+        if (outdegree[i] == indegree[i]+1) {
+            getEulerCircuit(i, circuit);
+            return circuit;
+        }
+    }
+    
+    // In case of having a cycle (=euler circuit),
+    for (int i=0; i<ALPHABETS; i++) {
+        // Just start at any vertex.
+        if (outdegree[i]) {
+            getEulerCircuit(i, circuit);
+            return circuit;
+        }
+    }
+    
+    // If failed,
+    return circuit;
+}
+
+// Verify if it has the euler circuit or path.
+bool checkEuler() {
+    int plus1 = 0; // The number of candidate start points.
+    int minus1 = 0; // The number of candidate end points.
+    for (int i=0; i<ALPHABETS; i++) {
+        int delta = outdegree[i] - indegree[i];
+        // The degree of all vertics should be -1, 1, or 0.
+        if (delta < -1 || 1 < delta) return false;
+        if (delta == 1) plus1++;
+        if (delta == -1) minus1++;
+    }
+    
+    // Start and end point should be only each one and should not be.
+    return (plus1 == 1 && minus1 == 1) || (plus1 == 0 && minus1 == 0);
+}
+
+// In directed graph, to have the euler circuit,
+// in-degree of a vertex should be the same as out-degree of that.
+void solve() {
+    cin >> n;
+    vector<string> words(n);
+    for (int i=0; i<n; i++) cin >> words[i];
+    makeGraph(words);
+    
+    if (!checkEuler()) {
+        cout << "IMPOSSIBLE" << endl;
+    } else {
+        vector<int> circuit = getEulerPathOrCircuit();
+        // If it can not visit all the edges, fail.
+        if (circuit.size() != words.size() + 1) {
+            cout << "IMPOSSIBLE" << endl;
+        } else {
+            reverse(circuit.begin(), circuit.end());
+            for (int i=1; i<circuit.size(); i++) {
+                int a = circuit[i-1], b = circuit[i];
+                if (i > 1) cout << " ";
+                cout << graph[a][b].back();
+                graph[a][b].pop_back();
+            }
+            cout << endl;
+        }
+    }
+}
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
